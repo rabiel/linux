d3578f772bdef3a9dd8284e01b8f5b46127e545e
Roi Dayan <roid@mellanox.com>
2017-12-24 14:59:16 +0200

    fixup! fixup! net/mlx5e: Allocate flow counter according to device capability
    Change-Id: Ide1418062c422aa1a67406b33423a26656f4bc83

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -2336,7 +2336,7 @@ int mlx5e_configure_flower(struct mlx5e_priv *priv, __be16 protocol,
 			goto err_free2;
 
 		if ((MLX5_CAP_ESW(esw->dev, counter_eswitch_affinity) == MLX5_COUNTER_SOURCE_ESWITCH) &&
-		    (flow->esw_attr->in_rep->vport != UPLINK_REP_INDEX))
+		    (flow->esw_attr->in_rep->vport != FDB_UPLINK_VPORT))
 			peer_flow->esw_attr->counter_dev = priv->mdev;
 		else
 			peer_flow->esw_attr->counter_dev = peer_priv->mdev;
